8d09f3532c5e43b18e6a9d689917e21f4d843b70
Junghs-cloud <rabbit4935@gmail.com>
2023-07-12 22:46:47 +0900

    07/12

---
+++ Silver/3/_15657.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int N, M;
+int* arr;
+int* answer;
+
+void backTracking(int depth, int index)
+{
+	if (depth == M)
+	{
+		for (int i = 0; i < M; i++)
+		{
+			cout << answer[i]<<" ";
+		}
+		cout << "\n";
+		return;
+	}
+	else
+	{
+		for (int i = index; i < N; i++)
+		{
+			answer[depth] = arr[i];
+			backTracking(depth + 1, i);
+		}
+	}
+}
+
+int main()
+{
+	cin >> N>>M;
+	arr = new int[N];
+	answer = new int[M];
+	for (int i = 0; i < N; i++)
+	{
+		cin >> arr[i];
+	}
+	sort(arr, arr + N);
+	backTracking(0, 0);
+	return 0;
+}
