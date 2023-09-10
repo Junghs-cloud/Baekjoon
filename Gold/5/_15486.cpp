1a0c8e46e6a103a074e0413d78584e568e37fd0b
Junghs-cloud <rabbit4935@gmail.com>
2023-09-10 22:54:49 +0900

    09/10

---
+++ Gold/5/_15486.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	int N;
+	cin >> N;
+	int* T = new int[N+1];
+	int* P = new int[N+1];
+	int* dp = new int[N + 1];
+	T[0] = 0;
+	P[0] = 0;
+	for (int i = 0; i <N + 1; i++)
+	{
+		dp[i] = 0;
+	}
+	for (int i = 0; i < N; i++)
+	{
+		cin >> T[i+1] >> P[i+1];
+	}
+	if (T[0] <= N)
+	{
+		dp[T[0]] = P[0];
+	}
+	for (int i = 1; i < N + 1; i++)
+	{
+		dp[i + T[i] - 1] = max(dp[i + T[i] - 1], dp[i - 1] + P[i]);
+		dp[i] = max(dp[i - 1], dp[i]);
+	}
+	cout << dp[N] << "\n";
+	return 0;
+}
