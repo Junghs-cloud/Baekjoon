17dbf7e9c3a67ca6a34a802b6bceaf51be956d28
Junghs-cloud <rabbit4935@gmail.com>
2025-01-16 23:10:54 +0900

    01/16 Silver3 26267

---
+++ Silver/3/_26267.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <map>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	map<int, long long> m;
+	int N;
+	int X, T, C;
+	map<int, long long>::iterator it;
+	long long answer = 0;
+	cin >> N;
+	for (int i = 0; i < N; i++)
+	{
+		cin >> X >> T >> C;
+		it = m.find(T - X);
+		if (it == m.end())
+		{
+			m.insert({T-X, C});
+			answer = max(answer, (long long) C);
+		}
+		else
+		{
+			it->second += C;
+			answer = max(answer, it->second);
+		}
+	}
+	cout << answer << "\n";
+	return 0;
+}
