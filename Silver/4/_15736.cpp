3bdb2b4b7cb555e26a09f03de226450ba942807a
Junghs-cloud <rabbit4935@gmail.com>
2025-06-28 22:55:23 +0900

    06/28 Silver4 15736

---
+++ Silver/4/_15736.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	long long N;
+	cin >> N;
+	long long answer = sqrt(N);
+	cout << answer << "\n";
+	return 0;
+}
